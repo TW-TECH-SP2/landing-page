3151e5d075bb0027869ff8c4f6d1ce95cef76d07
DSM5 <fatec-dsm5@academico.intra>
2026-05-13 20:13:59 -0300

    Atualizando pitch, imagens, textos, carrossel

--- js/index.js
+++ js/index.js
@@ -9,3 +9,31 @@ function getSticky() {
     navbar.classList.remove("sticky");
   }
 }
+// ===== CARROSSEL =====
+
+const track = document.querySelector(".carousel-track");
+const slides = document.querySelectorAll(".carousel-track img");
+const nextBtn = document.querySelector(".next");
+const prevBtn = document.querySelector(".prev");
+
+let index = 0;
+
+function updateCarousel() {
+  track.style.transform = `translateX(-${index * 100}%)`;
+}
+
+nextBtn.addEventListener("click", () => {
+  index = (index + 1) % slides.length;
+  updateCarousel();
+});
+
+prevBtn.addEventListener("click", () => {
+  index = (index - 1 + slides.length) % slides.length;
+  updateCarousel();
+});
+
+// autoplay
+setInterval(() => {
+  index = (index + 1) % slides.length;
+  updateCarousel();
+}, 5000);
